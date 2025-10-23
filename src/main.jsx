3cc59ad316bd81efe2a4a1fa610b57e4bb117956
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2025-10-23 21:44:55 +0100

    Add CSS Reset and web fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./styles/preflight.css";
+import "./styles/index.css";
 import routes from "./routes";
 
 const router = createBrowserRouter(routes);
